f6324ecf47b838cb778a05808067ae78148dc92e
Taalha3359 <abdurrahmantalha3359@gmail.com>
2025-08-31 12:19:00 +0600

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime, timedelta
 
 class UserDatabase:
     def __init__(self, file_path='data/users.json'):
@@ -7,7 +8,6 @@ class UserDatabase:
         self._ensure_data_file()
     
     def _ensure_data_file(self):
-        # Create folder if it doesn't exist
         os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
         if not os.path.exists(self.file_path):
             with open(self.file_path, 'w') as f:
@@ -23,12 +23,34 @@ class UserDatabase:
     
     def get_user(self, user_id):
         data = self._read_data()
-        return data.get(str(user_id))
+        user_data = data.get(str(user_id), {})
+        
+        # Set default values if not present
+        defaults = {
+            'total_score': 0,
+            'questions_answered': 0,
+            'premium_access': False,
+            'premium_until': None,
+            'is_admin': False,
+            'math': {'correct': 0, 'total': 0, 'topics': {}},
+            'english': {'correct': 0, 'total': 0, 'topics': {}},
+            'analytical': {'correct': 0, 'total': 0, 'topics': {}}
+        }
+        
+        for key, value in defaults.items():
+            if key not in user_data:
+                user_data[key] = value
+        
+        return user_data
     
     def create_user(self, user_id):
         data = self._read_data()
         user_data = {
             'total_score': 0,
+            'questions_answered': 0,
+            'premium_access': False,
+            'premium_until': None,
+            'is_admin': False,
             'math': {'correct': 0, 'total': 0, 'topics': {}},
             'english': {'correct': 0, 'total': 0, 'topics': {}},
             'analytical': {'correct': 0, 'total': 0, 'topics': {}}
@@ -41,3 +63,37 @@ class UserDatabase:
         data = self._read_data()
         data[str(user_id)] = user_data
         self._write_data(data)
+    
+    def increment_questions_answered(self, user_id):
+        user_data = self.get_user(user_id)
+        user_data['questions_answered'] = user_data.get('questions_answered', 0) + 1
+        self.update_user(user_id, user_data)
+        return user_data['questions_answered']
+    
+    def add_premium_access(self, user_id, days):
+        user_data = self.get_user(user_id)
+        if user_data['premium_until']:
+            current_end = datetime.fromisoformat(user_data['premium_until'])
+            new_end = current_end + timedelta(days=days)
+        else:
+            new_end = datetime.now() + timedelta(days=days)
+        
+        user_data['premium_until'] = new_end.isoformat()
+        user_data['premium_access'] = True
+        self.update_user(user_id, user_data)
+        return new_end
+    
+    def check_premium_status(self, user_id):
+        user_data = self.get_user(user_id)
+        if user_data['premium_until']:
+            premium_until = datetime.fromisoformat(user_data['premium_until'])
+            if datetime.now() > premium_until:
+                user_data['premium_access'] = False
+                user_data['premium_until'] = None
+                self.update_user(user_id, user_data)
+        return user_data['premium_access']
+    
+    def set_admin(self, user_id, is_admin):
+        user_data = self.get_user(user_id)
+        user_data['is_admin'] = is_admin
+        self.update_user(user_id, user_data)
